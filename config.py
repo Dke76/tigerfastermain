311252163da48fe86da006a07e4b312fa62320e2
Dke76 <156434263+Dke76@users.noreply.github.com>
2024-03-10 15:52:38 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) ACE 
+
+import os
+
+class Config(object):
+    # get a token from @BotFather
+    pass
+    """
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6729137267:AAFfMoCcoMfqqMgToHmef0ApLsvLS6lkKEc")
+    API_ID = int(os.environ["API_ID", 20319884]
+    API_HASH = os.environ["API_HASH", "637e3ba6357aa3ba2f3bf5742e0fd066"]
+    AUTH_USERS = "1183124209"
+
